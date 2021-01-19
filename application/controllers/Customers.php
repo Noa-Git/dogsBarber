e9524f7fafb5f3b4d9c8acc338136eaf9aad3a1d
Noa R <noa.ramon01@gmail.com>
2021-01-19 23:02:20 +0200

    Recreating register and login

--- application/controllers/Customers.php
+++ application/controllers/Customers.php
@@ -1,19 +1,9 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class Customers extends CI_Controller {
 
-/**
- * Description of Customer
- *
- * @author chen
- */
-class Customers extends CI_Controller{
     //put your code here
-    public function __construct(){
+    public function __construct() {
         parent::__construct();
         $this->load->model('Customers_model');
         $this->load->helper('url');
@@ -21,131 +11,94 @@ class Customers extends CI_Controller{
         $this->load->library('session');
         $this->load->library('form_validation');
     }
-    
-     public function index(){
-         $this->login();
-           // $data['Customer']=$this->Customers_model->get_customers();
-          //  print_r($data);
-           
-     }
-     
-     public function success() {
-         
-         echo '<h1> login successfull</h1>';
-         
-     }
 
-
-     public function login($error=null){
-      //  $this->load->view('templates/header');
-        //$this->load->view('templates/headerC');
-        $data['error']=$error;
-        $this->load->view('customers/login',$data);
-    
+    public function index() {
+        $this->login();
+        // $data['Customer']=$this->Customers_model->get_customers();
+        //  print_r($data);
     }
-     
-     public function register($info=null) {
-           $data['info']=$info;
-        $this->load->view('customers/register',$data);
-         
-     }
-     
-       public function save_customer(){
-        $this->form_validation->set_rules('id', 'id', 'required');
-        if ($this->form_validation->run() == FALSE) {
-            $error = ['message' => 'Id is required'];
-            $this->register($error);
-            return;
-        }
-        $this->form_validation->set_rules('name', 'name', 'required|max_length[20]');
-        if ($this->form_validation->run() == FALSE) {
-            $error = ['message' => 'Name can not be more than 20 charachters'];
-            $this->register($error);
-            return;
-        }
-        $this->form_validation->set_rules('password', 'password', 'required|min_length[6]');
-        if ($this->form_validation->run() == FALSE) {
-            $error = ['message' => 'Password have to contain at least 6 charachters'];
-            $this->register($error);
-            return;
-        }
-        $data = array(
-            'id' => $this->input->post('id'),
-            'name' => $this->input->post('name'),
+
+    public function success() {
+
+        echo '<h1> login successfull</h1>';
+    }
+
+    public function login($error = null) {
+        //  $this->load->view('templates/header');
+        //$this->load->view('templates/headerC');
+        $data['error'] = $error;
+        $this->load->view('customers/login', $data);
+    }
+
+    public function register($info = null) {
+        $data['info'] = $info;
+        $this->load->view('customers/register', $data);
+    }
+
+    public function save_customer() {  
+//        $this->form_validation->set_rules('name', 'name', 'required|max_length[20]');
+//        if ($this->form_validation->run() == FALSE) {
+//            $error = ['message' => 'Name can not be more than 20 charachters'];
+//            $this->register($error);
+//            return;
+//        }
+//        $this->form_validation->set_rules('password', 'password', 'required|min_length[6]');
+//        if ($this->form_validation->run() == FALSE) {
+//            $error = ['message' => 'Password have to contain at least 6 charachters'];
+//            $this->register($error);
+//            return;
+//        }
+//        $this->form_validation->set_rules('confirmPassword', 'confirm password', 'required|matches[password]');
+//        if ($this->form_validation->run() == FALSE) {
+//            $error = ['message' => 'Password does not match!'];
+//            $this->register($error);
+//            return;
+//        }
+        //preparing data for db
+         $data = array(
+            'email' => $this->input->post('email'),
+            'first_name' => $this->input->post('fname'),
+            'last_name' => $this->input->post('lname'),
+            'phone_number' => $this->input->post('phone'),
             'password' => $this->input->post('password')
         );
 
         $error = $this->Customers_model->save($data);
-        if ($error)
+        if ($error) {
             $this->register($error);
-        else {
+        } else {
+            $data['loggedin'] = '1';
+            $data['message'] = 'User Registered successfuly';
+            $data['code'] = 1;
+            $this->session->set_userdata($data);
             $this->login();
         }
-       
     }
-    
-          public function auth(){
 
-           $data = array(
-               'email' => $this->input->post('email'),
-               'password' => $this->input->post('password')
-                
-             );
-           
-            $check=$this->Customers_model->auth($data);
-          
-            if ($check==null){
-               $data['error']='User not found';
-               $this->login($data);
-            }
-            else{
-              $data['name']=$check[0]->name;
-              $data['loggedin']='1';
-              $this->session->set_userdata($data); 
-              redirect("Customers/success");
-            }
-   
-    }
-    
-   
-    
-    public function save_register(){
+    public function auth() {
         $data = array(
-               'email' => $this->input->post('email'), 
-               'first_name' => $this->input->post('fname'),
-               'last_name' => $this->input->post('lname'),
-               'phone_number' => $this->input->post('phone'),
-               'password' => $this->input->post('password')
-                
-         );
-        
-        $error=$this->Customers_model->save($data);
-        if ($error){
-            $this->register($error);
-        }
-        else{
-            $data['loggedin']='1';
-            $data['message']='User Registered successfuly';
-            $data['code']=1;
-            $this->session->set_userdata($data); 
-            $this->login();
-           
+            'email' => $this->input->post('email'),
+            'password' => $this->input->post('password')
+        );
+        $check = $this->Customers_model->auth($data);
+        if ($check == null) {
+            $data['error'] = 'Wrong user name or password';
+            $this->login($data);
+        } else {
+            $data['email'] = $check[0]->name;
+            $data['loggedin'] = '1';
+            $this->session->set_userdata($data);
+            redirect("Customers/success");
         }
     }
-    
-     public function logout()
-       {
-              $data = array(
-               'user',
-                'loggedin'  
-             );
-             $this->session->unset_userdata($data);
-             $this->login();
-    
-                         
-       }
-       
-   
-     
-}    
 
+    public function logout() {
+        $data = array(
+            'user',
+            'loggedin'
+        );
+        $this->session->unset_userdata($data);
+        $this->login();
+    }
+
+}
--- application/models/Customers_model.php
+++ application/models/Customers_model.php
@@ -1,51 +1,54 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class Customers_model extends CI_Model {
 
-/**
- * Description of Customer_model
- *
- * @author chen
- */
-class Customers_model extends CI_Model{
-     
-    public function __construct(){
+    public function __construct() {
         parent::__construct();
         $this->load->database();
     }
-    
-    public function get_customers(){
-    
-     $this->db->db_debug = FALSE; 
-     $error=null;
-     $query=$this->db->get('Customer');
-     if ($query){
-      return $query->result();
-      }
-      $error= $this->db->error();
-     return $error;      
 
+    public function get_customers() {
+
+        $this->db->db_debug = FALSE;
+        $error = null;
+        $query = $this->db->get('Customer');
+        if ($query) {
+            return $query->result();
+        }
+        $error = $this->db->error();
+        return $error;
+    }
+
+    public function save($data) {
+        $this->db->db_debug = FALSE;
+        $error = null;
+        $encrypted = $this->encrypt($data['password']);
+        $data['password'] = $encrypted;
+        if (!$this->db->insert('Customer', $data)) {
+            $error = $this->db->error();
+        }
+        return $error;
+    }
+
+    public function auth($data) {
+        $new_data = array('email'=>$data['email']);
+        $query = $this->db->get_where('Customer', $new_data);
+		if ($query->result()!=null) {
+			$res = $query->result();
+			$password = $res[0]->password;
+			$encrypted = $this->encrypt($data['password']);
+			if ($password == $encrypted) {
+				return $query->result();
+			}
+		}
+		return null;
     }
     
-    public function save($data)
-{
-      $this->db->db_debug=FALSE;
-      $error=null;
-      if(!$this->db->insert('Customer', $data)){
-          $error=$this->db->error();
-         
-      }
-      
-     return $error; 
-}
+    private function encrypt($password){
+    	//salt created by hashing the reversed password
+        $salt = md5(strrev($password));
+        $encrypted = md5($password.$salt);
+        return $encrypted;
+    }
 
-  public function auth($data){
-        $query = $this->db->get_where('Customer', $data);
-        return $query->result();
-
-     }
 }
