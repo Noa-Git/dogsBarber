709e7376517ed4752540330a713c0fbd2d790918
Chen Weitzman <chen@Chens-MacBook-Pro.local>
2021-02-05 17:24:07 +0200

    added orders model

--- application/models/Orders_model.php
+++ application/models/Orders_model.php
@@ -6,7 +6,84 @@ class Orders_model extends CI_Model
 	public function __construct() {
 		parent::__construct();
 		$this->load->database();
+                $this->db->db_debug = FALSE;
 	}
+        
+        public function get_orders() {
+            
+            $error = null;
+            $query = $this->db->get('Orders');
+            if ($query) {
+                return $query->result();
+            }
+            $error = $this->db->error();
+            return $error;
+        }
+        
+        public function get_orders_by_cust_id($cust_id) {
+            $error = null;
+            $data = array('customer_id' => $cust_id);
+	    $query = $this->db->get_where('Orders', $data);
+	     if ($query) {
+                return $query->result();
+            }
+            $error = $this->db->error();
+            return $error;
+            
+        }
+        
+        
+         public function save_order($data) {
+
+        $error = null;
+
+        if (!$this->db->insert('Orders', $data)) {
+            $error = $this->db->error();
+        }
+        return $error;
+    }
+    
+       public function save_orders_add($data) {
+
+        $error = null;
+
+        if (!$this->db->insert('Order_add', $data)) {
+            $error = $this->db->error();
+        }
+        return $error;
+    }
+
+         public function get_last_order($cust_id) {
+            
+            $error = null;
+            $this->db->order_by('order_date', 'DESC');
+            $query = $this->db->get_where('Orders',array('customer_id'=>$cust_id));
+            if ($query) {
+                return ($query->result())[0];
+            }
+            $error = $this->db->error();
+            return $error;
+        }  
+        
+        
+         public function get_pupolar_order() {
+            
+            $sql='SELECT service_name,counted 
+                FROM(
+                   SELECT COUNT(*)AS counted,service_id
+                    FROM Orders 
+                    GROUP BY service_id order by counted desc) as ms
+                    INNER JOIN Service on Service.id=ms.service_id'; 
+             
+            $error = null;
+            
+            $query = $this->db->query($sql);
+            if ($query) {
+                return ($query->result())[0];
+            }
+            $error = $this->db->error();
+            return $error;
+        }
 
 
 
--- application/models/Services_model.php
+++ application/models/Services_model.php
@@ -28,5 +28,17 @@ class Services_model extends CI_Model
 		$error = $this->db->error();
 		return $error;
 	}
+        
+        public function get_add_services_by_order_id($order_id){
+		$error = null;
+                $this->db->join('Order_add', 'Additional_services.id = Order_add.additional_services_id');
+		$query = $this->db->get_where('Additional_services',array('Orders_id'=>$order_id));
+                
+		if ($query) {
+			return $query->result();
+		}
+		$error = $this->db->error();
+		return $error;
+	}
 
 }
