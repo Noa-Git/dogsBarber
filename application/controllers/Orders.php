80e03862cb99e1aa63ccc101f0abb37c848915b3
Elad Bar Niv <elad.b@dynamicyield.com>
2021-02-07 19:17:53 +0200

    Added the whole functionality of getting available employees and available services by relative loction

--- application/controllers/Orders.php
+++ application/controllers/Orders.php
@@ -30,6 +30,7 @@ class Orders extends CI_Controller
 		$data['customer'] =$this->Customers_model->get_customer_by_id($id);
 		$data['dogs'] = $this->Dogs_model->get_dog_by_cust_id($id);
 		$data['add_services'] = $this->Services_model->get_add_services();
+		// To do: check services not null -> send to no_services_available
 		$data['services'] = $this->get_available_services();
 
 
@@ -64,28 +65,103 @@ class Orders extends CI_Controller
 	}
 
 	private function get_available_services(){
-		//dummy data until Elad's implementation
-		return array(
-			"1"=>array(
-				'employees'=>array(
-					array('employee_id'=>'1','employee_name'=>'Itamar'),
-					array('employee_id'=>'2','employee_name'=>'Elad')
-				),
-				'service_name'=>'Shower',
-				'price'=>'100'
-			),
-			"2"=>array(
-				'employees'=>array(
-					array('employee_id'=>'1','employee_name'=>'Itamar'),
-					array('employee_id'=>'3','employee_name'=>'moshe')
-				),
-				'service_name'=>'Haircut',
-				'price'=>'150'
-			)
-		);
+		//declare empty array
+		$avialable_employees_by_service = array();
+
+		//get geocode
+		$location_data = getLatitudeAndLongitude();
+		if ($location_data['error']) {
+			return null;
+		}
+
+		//get all employees from DB
+		$data['employees'] = $this->Employee_model->get_employees();
+		// get all services from DB
+		$data['services'] = $this->Services_model->get_services();
+
+		//create return array structure by services
+		foreach ($services as $service) {
+			$servie_id = $service->id;
+			$service_name = $service->name;
+			$service_price = $service->price;
+			$avialable_employees_by_service[$servie_id] = array( 'employees' => array(), 'price'=> $service_price, 'service_name' => $service_name);
+		}
+
+		// get available employees and their services
+		foreach ($emplyees as $employee) {
+
+			// calcDistance with each employee -> get a number which is the distance.
+			$distance = calc_distance($employee->latitude, $employee->longitude, $location_data->latitude, $location_data->longitude);
+
+			// if the number is lower than employee raduis -> the employee is available
+			if ( $distance <= $employee->radius ) {
+
+				$employee_id = $emplyee->id;
+				$employee_fiest_name = $employee->first_name;
+				$employee_last_name = $employee->last_name;
+				$employee_full_name = $employee_fiest_name." ".$employee_last_name;
+
+				//get employee's services
+				$data['tepm_services'] = $this->Employee_model->get_employee_services_by_emp_id($employee_id);
+
+				//push employee's services to the returned array
+				foreach ($temp_services as $temp_service) {
+					$service_id = $temp_service->service_id;
+					$avialable_employees_by_service[$service_id]['employees'] = array('employee_id' => $employee_id, 'employee_name' => $employee_full_name);
+				}
+			}
+		}
+
+		return $avialable_employees_by_service;
+
 	}
 
+	private function getLatitudeAndLongitude() {
+		// 1. get user address from db
+		$id = $this->session->id;
+		$data['address'] = $this->Address_model->get_address_by_cust_id($id);
+		$street = $address->street;
+		$city = $address->city;
 
+		// implement API call
+		$url = "https://api.positionstack.com/v1/forward?access_key=33576097aa621d30119b54a9621279d3&query=".urlecode($street).",".urlencode($city);
+		$response = file_get_contents($url);
 
+		if ($response) {
+
+			$location_response = json_decode($response, true);
+			$location_data['latitude'] = $location_response['data']['results'][0]['latitude'];
+			$location_data['longitude'] = $location_response['data']['results'][0]['longitude'];
+
+			return $location_data;
+		} else {
+
+			$data['error']= 1;
+			$data['message'] = 'Could not find Geocode for this address';
+
+			return $data;
+		}
+	}
+
+	private function calc_distance($employeeLatitude, $employeeLongitude, $customerLatitude, $ecustomerLongitude) {
+
+		// getting employee and customer latitude and longitude
+		$radEmpLat = deg2rad($employeeLatitude);
+		$radEmpLon = deg2rad($employeeLongitude);
+		$radCusLat = deg2rad($customerLatitude);
+		$radCusLon = deg2rad($ecustomerLongitude);
+
+		//set up data
+		$finalLong = $radEmpLon - $radCusLon;
+		$finalLati = $radEmpLat - $radCusLat;
+
+		// calculate distance
+		$val = pow(sin($finalLati/2),2) + cos($radCusLat) * cos($radEmpLat) * pow(sin($finalLong/2),2);
+		$res = 2 * asin(sqrt($val));
+		$radius = 3958.756;
+
+		// return a number
+		return ($res * $radius);
+	}
 
 }
