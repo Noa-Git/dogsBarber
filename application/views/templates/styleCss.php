15fcc7df28391f843075847e618ad68f0fcb04a0
Chen Weitzman <chen@Chens-MacBook-Pro.local>
2021-02-08 23:34:15 +0200

    added home page view

--- application/views/templates/styleCss.php
+++ application/views/templates/styleCss.php
@@ -1,7 +1,14 @@
 <!doctype html>
 <html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css'); ?>" >
-		<script src="https://code.jquery.com/jquery-3.5.1.min.js"
+    <head>
+        <meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css'); ?>" >        
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Arimo:wght@400;700&display=swap" rel="stylesheet">
+        <link rel=stylesheet type="text/css" href="CSS/homepage.css">
+    
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"
 				integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
 				crossorigin="anonymous"></script>
