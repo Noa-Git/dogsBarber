42e4ab6601864eb2e56136065eeb6782ddb9c8aa
Elad Bar Niv <elad.b@dynamicyield.com>
2021-02-09 21:30:57 +0200

    Added initial data to DB- customers, dogs, employees and orders

---
+++ DB Script/insert_customers.sql
@@ -0,0 +1,33 @@
+USE eyalet_BackEnd_Project_Chen;
+
+/*Cstomer 1*/
+INSERT INTO 'Customer' ('id', 'first_name', 'last_name', 'email', 'password', 'phone_nember') VALUES ('111111111', 'Anat', 'Lelior', 'anat@gmail.com', '1234', '050-1111111');
+INSERT INTO 'Address' ('customer_id', 'city', 'street', 'house_number', 'zip_code') VALUES ('111111111', 'Tel Aviv', 'Hayarkon', 10, 6560311);
+/*Cstomer 2*/
+INSERT INTO 'Customer' ('first_name', 'last_name', 'email', 'password', 'phone_nember') VALUES ('222222222', 'Miki', 'Kook', 'miki@hotmail.com', '1234', '050-2222222');
+INSERT INTO 'Address' ('customer_id', 'city', 'street', 'house_number', 'zip_code') VALUES ('222222222', 'Tel Aviv', 'Ben Yehuda', 132, 6340222);
+/*Cstomer 3*/
+INSERT INTO 'Customer' ('first_name', 'last_name', 'email', 'password', 'phone_nember') VALUES ('333333333', 'Omri', 'Ding', 'dingking@gmail.com', '1234', '050-3333333');
+INSERT INTO 'Address' ('customer_id', 'city', 'street', 'house_number', 'zip_code') VALUES ('333333333', 'Haifa', 'Tchernikhovski', 35, 3570901);
+/*Cstomer 4*/
+INSERT INTO 'Customer' ('first_name', 'last_name', 'email', 'password', 'phone_nember') VALUES ('444444444', 'Moti', 'Banana', 'bananas@gmai.com', '1234', '050-4444444');
+INSERT INTO 'Address' ('customer_id', 'city', 'street', 'house_number', 'zip_code') VALUES ('444444444', 'Hod Hasharon', 'Habanim', 23, 4526834);
+/*Cstomer 5*/
+INSERT INTO 'Customer' ('first_name', 'last_name', 'email', 'password', 'phone_nember') VALUES ('555555555', 'Jane', 'Bordoux', 'einav@gmail.com', '1234', '050-5555555');
+INSERT INTO 'Address' ('customer_id', 'city', 'street', 'house_number', 'zip_code') VALUES ('555555555', 'Yavne', 'Petel', 8, 8155336);
+/*Cstomer 6*/
+INSERT INTO 'Customer' ('first_name', 'last_name', 'email', 'password', 'phone_nember') VALUES ('666666666', 'Yossi', 'Revivo', 'yosrevivo@walla.co.il', '1234', '050-6666666');
+INSERT INTO 'Address' ('customer_id', 'city', 'street', 'house_number', 'zip_code') VALUES ('666666666', 'Shoham', 'Gazit', 3, 6082016);
+/*Cstomer 7*/
+INSERT INTO 'Customer' ('first_name', 'last_name', 'email', 'password', 'phone_nember') VALUES ('777777777' ,'David', 'Davidi', 'davidi@hitmail.com', '1234', '050-7777777');
+INSERT INTO 'Address' ('customer_id', 'city', 'street', 'house_number', 'zip_code') VALUES ('777777777', 'Acre', 'Herzl', 33, 2420336);
+/*Cstomer 8*/
+INSERT INTO 'Customer' ('first_name', 'last_name', 'email', 'password', 'phone_nember') VALUES ('888888888', 'Yossef', 'Choen', 'ycohen@gmail.com', '1234', '050-8888888');
+INSERT INTO 'Address' ('customer_id', 'city', 'street', 'house_number', 'zip_code') VALUES ('888888888', 'Netanya', 'David Remez', 21, 4240603);
+/*Cstomer 9*/
+INSERT INTO 'Customer' ('first_name', 'last_name', 'email', 'password', 'phone_nember') VALUES ('9999999999', 'Nurit', 'Levi', 'nirutlevi@gmail.com', '1234', '050-8888888');
+INSERT INTO 'Address' ('customer_id', 'city', 'street', 'house_number', 'zip_code') VALUES ('9999999999', 'Beer Sheva', 'Felix', 14, 8483456);
+/*Cstomer 10*/
+INSERT INTO 'Customer' ('first_name', 'last_name', 'email', 'password', 'phone_nember') VALUES ('101010101', 'Itay', 'Harel', 'itharel@gmail.com', '1234', '050-1010101');
+INSERT INTO 'Address' ('customer_id', 'city', 'street', 'house_number', 'zip_code') VALUES ('101010101', 'Ashdod', 'Kakal', 44, 7746136);
+
---
+++ DB Script/insert_orders.sql
@@ -0,0 +1,28 @@
+USE eyalet_BackEnd_Project_Chen;
+
+/*Order 1*/
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (1, 1, '111111111', '2021-02-09', 150);
+/*Order 2*/
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (2, 3, '222222222', '2021-02-09', 100);
+/*Order 3*/
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (3, 2, '333333333', '2021-02-09', 200);
+/*Order 4*/
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (4, 1, '444444444', '2021-03-01', 150);
+/*Order 5*/
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (5, 2, '555555555', '2021-03-01', 200);
+/*Order 6*/
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (6, 3, '666666666', '2021-03-01', 100);
+/*Order 7*/
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (7, 1, '777777777', '2021-04-01', 150);
+/*Order 8*/
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (8, 2, '888888888', '2021-04-01', 200);
+/*Order 9*/
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (9, 3, '9999999999', '2021-04-01', 100);
+/*Order 10*/
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (10, 1, '101010101', '2021-02-27', 150);
+/*Order 11*/
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (2, 3, '666666666', '2021-02-27', 100);
+/*Order 12*/
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (4, 2, '9999999999', '2021-02-27', 200);
+/*Order 13*/
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (6, 2, '333333333', '2021-02-27', 200);
