6536a8d50d461b2249f894914ac08d310f2854b2
Elad Bar Niv <elad.b@dynamicyield.com>
2021-02-10 18:01:47 +0200

    Adjust data script to minor DB structure changes

--- DB Script/insert_orders.sql
+++ DB Script/insert_orders.sql
@@ -1,28 +1,28 @@
 USE eyalet_BackEnd_Project_Chen;
 
 /*Order 1*/
-INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (1, 1, '111111111', '2021-02-09', 150);
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price', 'dog_id') VALUES (1, 1, '111111111', '2021-02-09', 150, 1);
 /*Order 2*/
-INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (2, 3, '222222222', '2021-02-09', 100);
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price', 'dog_id') VALUES (2, 3, '222222222', '2021-02-09', 100, 2);
 /*Order 3*/
-INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (3, 2, '333333333', '2021-02-09', 200);
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price', 'dog_id') VALUES (3, 2, '333333333', '2021-02-09', 200, 3);
 /*Order 4*/
-INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (4, 1, '444444444', '2021-03-01', 150);
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price', 'dog_id') VALUES (4, 1, '444444444', '2021-03-01', 150, 4);
 /*Order 5*/
-INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (5, 2, '555555555', '2021-03-01', 200);
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price', 'dog_id') VALUES (5, 2, '555555555', '2021-03-01', 200, 5);
 /*Order 6*/
-INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (6, 3, '666666666', '2021-03-01', 100);
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price', 'dog_id') VALUES (6, 3, '666666666', '2021-03-01', 100, 6);
 /*Order 7*/
-INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (7, 1, '777777777', '2021-04-01', 150);
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price', 'dog_id') VALUES (7, 1, '777777777', '2021-04-01', 150, 7);
 /*Order 8*/
-INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (8, 2, '888888888', '2021-04-01', 200);
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price', 'dog_id') VALUES (8, 2, '888888888', '2021-04-01', 200, 8);
 /*Order 9*/
-INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (9, 3, '9999999999', '2021-04-01', 100);
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price', 'dog_id') VALUES (9, 3, '9999999999', '2021-04-01', 100, 9);
 /*Order 10*/
-INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (10, 1, '101010101', '2021-02-27', 150);
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price', 'dog_id') VALUES (10, 1, '101010101', '2021-02-27', 150, 10);
 /*Order 11*/
-INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (2, 3, '666666666', '2021-02-27', 100);
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price', 'dog_id') VALUES (2, 3, '666666666', '2021-02-27', 100, 6);
 /*Order 12*/
-INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (4, 2, '9999999999', '2021-02-27', 200);
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price', 'dog_id') VALUES (4, 2, '9999999999', '2021-02-27', 200, 9);
 /*Order 13*/
-INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price') VALUES (6, 2, '333333333', '2021-02-27', 200);
+INSERT INTO 'Orders' ('employee_id', 'service_id', 'customer_id', 'order_date', 'total_price', 'dog_id') VALUES (6, 2, '333333333', '2021-02-27', 200, 3);
